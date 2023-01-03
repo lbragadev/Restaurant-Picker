52b00d7d5c8fab1868fa1578f073f9b46e04e10e
lbragadev <106041646+lbragadev@users.noreply.github.com>
2023-01-03 14:35:14 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+
+#set up requirements.txt
+
+#add click dependency
+    #import CLICK 
+    #use click cli
+
+# design restaurant class
+# create restaurant objects
+# implement pick restaurant method
+# implement add restaurant method
+# implement delete restaurant method
+
+
+
+def pickRestaurant():
+        print("PICKING RESTAURANT")
+
+
+def main():
+    print("hello world")
+    pickRestaurant()
+    print("END!")
+
+main()
